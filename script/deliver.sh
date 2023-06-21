4f217dfa58f76a9c4261c938af43f907484ba19e
Wamgleb <kalinchukgleb1@gmail.com>
2023-06-21 21:24:22 +0300

    Add delivery script

---
+++ script/deliver.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo 'The following command runs and outputs the execution of your Java'
+echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+java -jar target/*.jar
